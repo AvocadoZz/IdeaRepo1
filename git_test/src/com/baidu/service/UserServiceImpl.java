6ccd4a199ece8e192d565c45224aac7b23e77ce9
Avocado <kctwlne317@163.com>
2019-04-12 19:17:38 +0800

    修改测试类，给user对象赋值

--- git_test/src/com/baidu/service/UserServiceImpl.java
+++ git_test/src/com/baidu/service/UserServiceImpl.java
@@ -5,6 +5,8 @@ import com.baidu.domain.User;
 public class UserServiceImpl {
 	public static void main(String[] args) {
 		User user = new User();
+		user.setName("Avocado");
+		user.setAge(12);
 		user.show();
 	}
 }
