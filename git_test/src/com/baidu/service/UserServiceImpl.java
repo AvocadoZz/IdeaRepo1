ed7de18d2f4dbf6c1be77d7e83ad166ae0122abb
Avocado <kctwlne317@163.com>
2019-04-12 19:13:36 +0800

    Push My First Project

---
+++ git_test/src/com/baidu/service/UserServiceImpl.java
@@ -0,0 +1,10 @@
+package com.baidu.service;
+
+import com.baidu.domain.User;
+
+public class UserServiceImpl {
+	public static void main(String[] args) {
+		User user = new User();
+		user.show();
+	}
+}
